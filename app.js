9f9f317277f8bd8dadb4507a3061fd0a8cb6f3c5
Cody Finn <codyfinn@protonmail.com>
2018-11-25 17:15:43 -0500

    added connection listener to socket server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const Lobby = require('./src/lobby');
 
 app.get('/lobby', (req, res) => {
--- src/lobby.js
+++ src/lobby.js
@@ -4,14 +4,15 @@ const Player = require('./player');
 class Lobby {
 
     constructor(server, path) {
-        this.server = new WebSocket.Server({server});
+        this.server = new WebSocket.Server({server: server, path: path});
+        server.on('connection', this.onConnection.bind(this));
         this.players = new Array();
     }
 
     onConnection(ws) {
         console.log("player requesting a connection");
         if(this.players.length < 10) {
-            let player = new Player(ws, lobby);
+            let player = new Player(ws, this);
             let player_id = this.players.push(player);
             player.id = player_id;
         }
