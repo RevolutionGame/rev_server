212ac67ced5ba86801915e1f1b4c68c4f09587f2
Cody Finn <codyfinn@protonmail.com>
2018-11-25 16:50:57 -0500

    added route back for getting path of server

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = process.env.PORT;
 const Lobby = require('./src/lobby');
 
+app.get('/lobby', (req, res) => {
+    res.send(lobby.server.options.path);
+});
+
 const server = app.listen(port, () => console.log(`Revolution Server up and listening on port ${port}`));
 const lobby = new Lobby(server, "/lobby");
 
