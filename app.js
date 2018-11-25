2a77e0fe5e67e64220a2b48c442efa664e1aff96
Cody Finn <codyfinn@protonmail.com>
2018-11-25 16:55:23 -0500

    added cors and full url to lobby route

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = process.env.PORT;
 const Lobby = require('./src/lobby');
 
 app.get('/lobby', (req, res) => {
-    res.send(lobby.server.options.path);
+		res.append('Access-Control-Allow-Origin', '*');
+		res.send(`ws://infinite-woodland-97233.herokuapp.com${lobby.options.path}`);
 });
 
 const server = app.listen(port, () => console.log(`Revolution Server up and listening on port ${port}`));
