e09ac6e4ce3b6f0e45da52668dbf8f9e31d7a243
Cody Finn <codyfinn@protonmail.com>
2018-11-25 16:47:08 -0500

    added path for lobby

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const port = process.env.PORT;
 const Lobby = require('./src/lobby');
 
 const server = app.listen(port, () => console.log(`Revolution Server up and listening on port ${port}`));
-const lobby = new Lobby(server);
+const lobby = new Lobby(server, "/lobby");
 
