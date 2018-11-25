561d433e1a7786c771648f3d2603f9e1f7f02511
Cody Finn <codyfinn@protonmail.com>
2018-11-25 17:30:45 -0500

    removed route for getting lobby url

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const app = express();
 const port = process.env.PORT || 8080;
 const Lobby = require('./src/lobby');
 
-app.get('/lobby', (req, res) => {
-		res.append('Access-Control-Allow-Origin', '*');
-		res.send(`ws://infinite-woodland-97233.herokuapp.com${lobby.server.options.path}`);
-});
-
 const server = app.listen(port, () => console.log(`Revolution Server up and listening on port ${port}`));
 const lobby = new Lobby(server, "/lobby");
 
